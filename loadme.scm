58aac73bff1e9f1abff91055c7c498ac477f8ee6
Darius Bacon <darius@wry.me>
2016-08-09 03:16:56 -0400

    cleanup

--- loadme.scm
+++ loadme.scm
@@ -1,14 +1,10 @@
-(load "util.scm")
-(load "read.scm")
-;(snarf "later/new.scm" squeam-read)  ; or readtest.scm?
-(define eg-program (snarf "eg/compact-lambda.scm" squeam-read))
-(load "parse.scm")
-(map parse-exp eg-program)
-
 (define (report x)
   (write x)
   (newline))
 
+(load "util.scm")
+(load "read.scm")
+(load "parse.scm")
 (load "terp.scm")
 
 (report (interpret 42))
