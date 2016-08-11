ce0d0b91ebd1583a3c620e175eb2d7bc4a2fed40
Darius Bacon <darius@wry.me>
2016-08-11 01:54:32 -0400

    add nqueens untested but meant to test bdd.scm

---
+++ eg/nqueens.scm
@@ -0,0 +1,70 @@
+;; The N-queens problem as a BDD.
+;; XXX untested
+
+;; TODO design command-line interfacing
+(define (main argv)
+  (match argv
+    ((_ n) (queens (number<-string n)))
+    ((prog @_) (format "Usage: %s board-size" prog))))
+
+(define (queens n)
+  (match (satisfy (queens-problem n) 1)
+    (#no (display "none\n"))
+    (env (show-board n env))))
+
+(define (show-board n env)
+  (for each! ((row (make-board n)))
+    (for each! ((var row))
+      (format "%s " (".Q" (env var))))
+    (newline)))
+
+(define (queens-problem n)
+  (conjoin (for each ((r (range<- n)))
+             (disjoin (for each ((c (range<- n)))
+                        (place-queen n r c))))))
+
+(define (conjoin nodes) (foldr1 bdd-and nodes))
+(define (disjoin nodes) (foldr1 bdd-or  nodes))
+
+(define (place-queen n r c)
+
+  (let env (map<-))
+
+  (define (exclude rr cc)
+    (when (and (<= 0 rr) (< rr n)
+               (<= 0 cc) (< cc n))
+      (env .set! (queen n rr cc) #no)))
+  
+  (for each! ((cc (range<- n)))
+    (exclude r cc))
+  (for each! ((rr (range<- n)))
+    (exclude rr c))
+  (for each! ((dd (range<- (+ (- n) 1) n)))
+    (exclude (+ r dd) (+ c dd))
+    (exclude (+ r dd) (- c dd))
+    (exclude (- r dd) (+ c dd))
+    (exclude (- r dd) (- c dd)))
+  (env .set! (queen n r c) #yes)
+
+  (match-env env))
+
+(define (match-env env)
+  ;; Return a BDD that evaluates to 1 just when every variable in env
+  ;; has its given value.
+  ;; TODO checkme
+  (for foldl ((tree lit1)
+              ((var value) (sort env.items {reverse})))
+    (if value
+        (build-choice var lit0 tree)
+        (build-choice var tree lit0))))
+
+(define (queen n r c)
+  ;; The variable for a queen at (row r, column c) in an n*n board.
+  (+ 1 (* n r) c))
+
+(define (make-board n)
+  ;; Return a 2-d array of distinct variables: each means there's a
+  ;; queen at its position.
+  ;; XXX make it a vector instead of a list?
+  (for each ((r (range<- n)))
+    (range<- (+ 1 (* r n)) (+ 1 (* (+ r 1) n)))))
