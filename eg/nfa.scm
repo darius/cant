2c08e0fca2c0f0081d98464864f59f4a8546ccf7
Darius Bacon <darius@wry.me>
2016-08-12 22:53:45 -0400

    get regex parser working

--- eg/nfa.scm
+++ eg/nfa.scm
@@ -11,60 +11,41 @@
 (define (accept c) empty-set)
 (define ((expect ch succs) c) (if (= ch c) succs empty-set))
 
-(let chain<- compose)
-(define ((lit<- ch) succs)  (set<- (expect ch succs)))
-(define ((alt<- r s) succs) (union (r succs) (s succs)))
+(let empty-set (set<-))
+
+(define ((lit<- ch) succs)    (set<- (expect ch succs)))
+(define ((alt<- r s) succs)   (union (r succs) (s succs)))
+(define ((chain<- r s) succs) (r (s succs)))
 (define ((star<- r) succs)
   (let my-succs succs.diverge)
   (my-succs .union! (r my-succs))
   my-succs)
 
-(make set<-             ;XXX this name is better saved for frozen sets
-  (() (hash-set<-))
-  ((val @vals) ;XXX would be nicer as (@vals) but that still matches non-lists
-   (let s (hash-set<-))
-   (s .add-all! `(,val ,@vals))
-   s))
-
-(define (hash-set<-)
-  (let map (map<-)) ;TODO would be nice to avoid storing all the #yes values
-  (make hash-set
-    ({.keys}          map.keys)
-    ({.maps? key}     (map .maps? key))
-    ({.diverge}       (call set<- map.keys)) ;TODO tune
-    ({.add! key}      (map .set! key #yes))
-    ({.add-all! vals} (for each! ((v vals)) (hash-set .add! v)))
-    ({.union! other}  (hash-set .add-all! other.keys))
-    ({.empty?}        map.empty?)
-    ;; XXX fill in rest of set interface (just the map interface, I guess)
-    ))
-
-(let empty-set (set<-))
-
-(define (union set1 set2)      ;XXX name clash with lambdacompiler.scm
-  (let result set1.diverge)
-  (result .union! set2)
-  result)
-
 
 ;; Parser
-;; XXX buggy
 
 (let regex-parser
   (hide
-   (let primary (delay (given ()
+   (let primary (delay (define (p)
                          (seclude
                           (either (then (lit-1 #\() exp (lit-1 #\)))
-                                  (then any-1 (feed lit<-))))))) ;XXX any-1 is overly permissive
+                                  (then (invert (either (lit-1 #\))
+                                                        (lit-1 #\|)
+                                                        (lit-1 #\*)))
+                                        any-1
+                                        (feed (given (s) (lit<- (s 0))))))))))
    (let factor (seclude
                 (then primary
-                      (maybe (either (then (lit-1 #\*) (feed star<-)))))))
-   (let term (delay (given ()
+                      (maybe (either (then (lit-1 #\*)
+                                           (feed star<-)))))))
+   (let term (delay (define (t)
                       (seclude
-                       (then factor (many (then term (feed chain<-))))))))
-   (let exp (delay (given ()
+                       (then factor (many (then term
+                                                (feed chain<-))))))))
+   (let exp (delay (define (e)
                      (seclude
-                      (either (then term (many (then (lit-1 #\|) exp (feed alt<-))))
+                      (either (then term (many (then (lit-1 #\|) exp
+                                                     (feed alt<-))))
                               empty)))))
    (then exp (invert skip-any-1))))
 
@@ -74,7 +55,9 @@
 
 ; TODO more tests
 
-(print (re-match (lit<- #\A) "hello"))
-(print (re-match (lit<- #\A) "A"))
+(let r ((regex-parser "a(b|c)*d" 0 0 '()) .result))
 
-;(print (re-match (parse-regex "B") "B"))
+(print (re-match r "ad"))
+(print (re-match r "ab"))
+(print (re-match r "abcd"))
+(print (re-match r "B"))
