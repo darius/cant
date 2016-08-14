2b1d1b8b1a2c76af532498c1a55c0098886beec9
Darius Bacon <darius@wry.me>
2016-08-14 17:33:56 -0400

    answer continuations via .answer, for clarity and to not conflict with list-trait

--- eg/test-continuations.scm
+++ eg/test-continuations.scm
@@ -3,7 +3,7 @@
 (define (on-error-have-fun k plaint @values)
   (call on-error-traceback `(,k ,plaint ,@values))
   (display "Now continuing with 42") (newline)
-  (k 42))  ; TODO (k .answer 42)
+  (k .answer 42))
 
 (the-signal-handler-box .^= on-error-have-fun)
 (print (+ 1 (error "I wish to complain" 'life 'is 'terrible)))
