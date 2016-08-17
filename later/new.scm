698b3f99a862bbdb3a7e0b9d7cf8fbd7e8599aff
Darius Bacon <darius@wry.me>
2016-08-16 23:18:10 -0400

    extract spelling

--- later/new.scm
+++ later/new.scm
@@ -329,67 +329,6 @@
      (call (addressee .evaluate r) cue (map '.evaluate operands)))))
 
 
-;; spell1.scm
-;; Norvig's (simpler) spelling corrector
-;; http://norvig.com/spell-correct.html
-;; TODO: try imitating https://en.wikibooks.org/wiki/Clojure_Programming/Examples/Norvig_Spelling_Corrector
-;; TODO: uses pattern matching
-
-(define (correct word)
-  (let candidates (or (if-any (known `(,word)))
-                      (if-any (known (edits1 word)))
-                      (if-any (known-edits2 word))
-                      `(,word)))
-  (max-by candidates (given (w) (NWORDS .get w 0))))
-
-(define (if-any xs)
-  (if xs.empty? #no xs))
-
-(define (known words)  ;TODO: iter instead of list? set comprehension?
-  (set<-list (for filter ((w words))
-               (NWORDS .maps? w))))
-
-(define (known-edits2 word)
-  (set<-list (for gather ((e1 (edits1 word)))
-               (for filter ((e2 (edits1 e1)))
-                 (NWORDS .maps? e2)))))
-
-(define (edits1 word)
-  (let splits     (for each ((i (range<- (+ word.count 1))))
-                    `(,(word .slice 0 i)
-                      ,(word .slice i))))
-  (let deletes    (for each (((a b) (for filter (((a b) splits))
-                                      (not b.empty?))))
-                    (chain a (b .slice 1))))
-  (let transposes (for each (((a b) (for filter (((a b) splits))
-                                      (< 1 b.count))))
-                    (chain a (string<- (b 1) (b 0)) (b .slice 2))))
-  (let replaces   (for gather ((a b) splits)
-                    (if b.empty?
-                        '()
-                        (for each ((c alphabet))
-                          (chain a (string<- c) (b .slice 1))))))
-  (let inserts    (for gather ((a b) splits)
-                    (for each ((c alphabet))
-                      (chain a (string<- c) b))))
-  (set<-list (chain deletes transposes replaces inserts)))
-
-(let alphabet "abcdefghijklmnopqrstuvwxyz")
-
-(define (train features)
-  (let model (map<-))
-  (for each! ((f features))
-    (model .set! f (+ 1 (model .get f 1))))
-  model)
-
-(define (words<-string string)
-;;  (re:findall "[a-z]+" string.lowercase))  ;TODO
-  string.lowercase.split)
-
-(let NWORDS (train (words<-string (for with-open-file ((source "big.txt"))
-                                    source.read-all)))) ;TODO
-
-
 ;; star2.scm
 ;; Like https://github.com/darius/regexercise_solutions/blob/master/star.py
 ;; ~/git/regexercise_solutions/star.py
