6450431c33684b14ba8d85693676bf08de964646
Darius Bacon <darius@wry.me>
2016-08-18 00:53:16 -0400

    tweaks

--- lib/dd.scm
+++ lib/dd.scm
@@ -1,4 +1,7 @@
 ;; Decision diagrams
+;; Choice nodes select a branch by the value of a variable, from 0 to
+;; #branches-1. Each variable is also represented by a number, its 'rank'
+;; -- lower rank numbers are placed nearer the root of the tree.
 ;; XXX untested
 
 (import (use "lib/memoize.scm") memoize)
@@ -6,6 +9,7 @@
 (let infinity 999999)  ;; N.B. we don't have floats yet
 
 (define (constant<- value)
+  (assert value "You can't use #no as a constant value -- it's reserved") ;XXX well, fix it
   (make
     ({.rank}           infinity)
     ({.constant-value} value)
@@ -20,17 +24,16 @@
   (assert (< rank infinity))
   (make choice
     ({.rank}           rank)
-    ({.constant-value} #no)
+    ({.constant-value} #no) ; The reason for the above reserving of #no.
     ({.evaluate env}   ((branches (env rank)) .evaluate env))
     ({.branches}       branches)
-    ({.choose nodes}
-     (case ((all-same? nodes)
-            (nodes 0))
-           ((= (each '.constant-value nodes)
-               (as-list nodes.keys))
-            choice)
-           (else
-            (memo-choice choice nodes))))))
+    ({.choose nodes}   (case ((all-same? nodes)
+                              nodes.first)
+                             ((= (each '.constant-value nodes)
+                                 (as-list nodes.keys))
+                              choice)
+                             (else
+                              (memo-choice choice nodes))))))
 
 (let memo-node (memoize choice<-))
 
@@ -40,29 +43,26 @@
 (let memo-choice
   (memoize
    (given (node branches)
-     (let top (arg-min (cons node branches) '.rank))
+     (let top (arg-min `(,node ,@branches) '.rank))
      (let rank top.rank)
      (make-node rank
-                (for each ((c (range<- top.branches.count)))
-                  ((subst rank c node)
-                   .choose (subst-each rank c branches)))))))
+                (for each ((c top.branches.keys))
+                  ((subst rank c node) .choose (subst-each rank c branches)))))))
 
 (define (make-node rank branches)
   (if (all-same? branches)
-      (branches 0)
+      branches.first
       (memo-node rank branches)))
 
 (define (subst-each rank value nodes)
   (for each ((e nodes)) (subst rank value e)))
 
 (define (subst rank value node)
-  (case ((< rank node.rank)
-         node) ; N.B. node must be a constant, iff we arrived here (XXX why?)
-        ((= rank node.rank)
-         (node.branches value))
-        (else
-         (make-node node.rank
-                    (subst-each rank value node.branches)))))
+  (match (rank .compare node.rank)
+    (-1 node) ; N.B. node must be a constant, iff we arrived here (XXX why?)
+    ( 0 (node.branches value))
+    (+1 (make-node node.rank
+                   (subst-each rank value node.branches)))))
 
 (define (valid? node)
   (not (satisfy node 0)))
