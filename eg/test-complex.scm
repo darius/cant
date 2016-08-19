6efa088bd9da479aded1036470915e0645a9352e
Darius Bacon <darius@wry.me>
2016-08-19 01:50:14 -0400

    test c/

--- eg/test-complex.scm
+++ eg/test-complex.scm
@@ -7,7 +7,10 @@
 (let l {complex 1 0})
 (let i {complex 0 1})
 
+(let hypot (c+ (c- z (c- z (c+ l (c+ l l))))
+               (c* i (c+ l (c+ l (c+ l l ))))))
+
 (format "0+0 = %w\n" (c+ z z))
 (format "i*i = %w\n" (c* i i))
-(format "|3+4i| = %w\n" (c-abs (c+ (c- z (c- z (c+ l (c+ l l))))
-                                   (c* i (c+ l (c+ l (c+ l l )))))))
+(format "|3+4i| = %w\n" (c-abs hypot))
+(format "z/z = %w\n" (c/ hypot hypot))
