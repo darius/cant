23c3a720178365a6b85ecdb6b21bde20edd48d04
Darius Bacon <darius@wry.me>
2017-03-06 19:49:29 -0800

    add echo

---
+++ eg/echo.scm
@@ -0,0 +1,29 @@
+;; To check how sturm interprets your keystrokes.
+;; We quit on Q instead of the escape key because there are still
+;; unmapped keys with escape sequences.
+
+(import (use "lib/sturm")
+  cbreak-mode get-key render cursor)
+
+(define (main)
+  (cbreak-mode run))
+
+(define (run)
+  (let strokes (fillvector<-))
+  (begin running ()
+    (let lines (text-wrap (" " .join strokes)     ;XXX does this work
+                          num-cols))
+    (render '("Hit some keys; or hit capital Q to quit.\n\n"
+              ,("\n" .join lines)
+              ,cursor))
+    (let key (get-key))
+    (unless (= key #\Q)
+      (strokes .push! ("%w" .format key))
+      (running))))
+
+;;XXX stubs
+(let num-cols 80)
+(define (text-wrap str width)
+  `(,str))      
+
+(export main run)
