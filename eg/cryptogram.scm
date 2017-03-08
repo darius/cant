ce20fdc011706b5510e77dd62a61b61a2a60b080
Darius Bacon <darius@wry.me>
2017-03-07 18:45:52 -0800

    flesh out cryptogram some more

--- eg/cryptogram.scm
+++ eg/cryptogram.scm
@@ -32,6 +32,7 @@
   )
 
 (define (puzzle cryptogram)
+
   (let code (for filter ((ch cryptogram)) ch.letter?))
   (assert (not code.empty?))
   (let decoder
@@ -53,4 +54,62 @@
         (unless (= #\space (decoder (code cursor-at.^)))
           (shifting)))))
 
-  XXX)
+  (define (shift-line offset)
+    (let next-line ((+ (line-number cursor-at.^) offset)
+                    .modulo lines.count))
+    (cursor .^= (line-starts next-line)))
+
+  (define (line-number pos)
+    (let items
+      (for filter (((i start) line-starts.items))
+        (and (<= start pos) (< pos (line-starts (+ i 1))))))
+    (match items.first                  ;TODO should be lazy
+      ((i _) i)))
+
+  (let line-starts
+    (running-sum (for each ((line lines))
+                   ((filter '.letter? line) .count))))
+
+  (define (view show-cursor?)
+    XXX)
+
+  (begin playing ()
+    (render (view #yes))
+    (let key (get-key))
+    (match key
+      ('esc       (render (view #no)))
+      ('home      (cursor-at .^= 0)                 (playing))
+      ('end       (cursor-at .^= (- code.count 1))  (playing))
+      ('left      (shift-by -1)                     (playing))
+      ('right     (shift-by  1)                     (playing))
+      ('up        (shift-line -1)                   (playing))
+      ('down      (shift-line  1)                   (playing))
+      (#\tab      (shift-to-space)                  (playing))
+      ('backspace (shift-by -1)    (jot #\space)    (playing))
+      ('del       (jot #\space)    (shift-by 1)     (playing))
+      (_
+       (when (printables .maps? key)
+         (jot key)
+         (shift-by 1))
+       (playing)))))
+
+;; Expand tabs; blank out other control characters.
+(define (clean str)
+  (let r (fillvector<-))
+  (for each! ((ch str))
+    (case ((= ch #\tab)
+           (begin padding ()
+             (r .push! #\space)
+             (unless (= 0 (r.count .modulo 8))
+               (padding))))
+          ((< ch.code 32)
+           (r .push! #\space))
+          (else
+           (r .push! ch))))
+  (string<-list r))
+
+(define (running-sum numbers)
+  (let sums (fillvector<- 0))
+  (for each! ((n numbers))
+    (sums .push! (+ sums.last n)))
+  sums)
