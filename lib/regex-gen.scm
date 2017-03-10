1867ea714f86d0109294d97b950a1bc67c901684
Darius Bacon <darius@wry.me>
2017-03-10 11:50:08 -0800

    tweak

--- lib/regex-gen.scm
+++ lib/regex-gen.scm
@@ -1,10 +1,13 @@
 ;; Generate strings that match a regex.
 ;; From Udacity CS212 Python code by Peter Norvig.
+;; Glossary:
+;;    r, s   regex (i.e. a thing built by the constructors below)
+;;    Ns     set of nonnegative integers (i.e. lengths)
 
-;; Return the strings matching regex whose length is in Ns.
-(to (generate regex Ns)
-  (sort-by-key (regex Ns)
-               (given (s) `(,s.count ,s))))
+;; Return the strings matching r whose length is in Ns.
+(to (generate r Ns)
+  (sort-by-key (r Ns)
+               (given (str) `(,str.count ,str))))
 
 (let none   (set<-))
 (let just-0 (set<- 0))
@@ -14,44 +17,44 @@
   (given (Ns)
     (if (Ns .maps? str.count) just-str none)))
 
-(to (star x)
-  (let x1 (nonempty x))
-  (begin x1* ()
+(to (star r)
+  (let r1 (nonempty r))
+  (begin r1* ()
     (given (Ns)
-      ((maybe (then x1 (x1*))) Ns))))
+      ((maybe (then r1 (r1*))) Ns))))
 
-(to (maybe x) (either empty x))
+(to (maybe r) (either empty r))
 
-(to ((nonempty x) Ns) (x (Ns .difference just-0)))
-(to ((either x y) Ns) ((x Ns) .union (y Ns)))
+(to ((nonempty r) Ns) (r (Ns .difference just-0)))
+(to ((either r s) Ns) ((r Ns) .union (s Ns)))
 
 (to (one-of chars)
   (let set (call set<- chars))
   (given (Ns)
     (if (Ns .maps? 1) set null)))
 
-(to ((then x y) Ns)
-  ;; Return the set of matches to xy whose total length is in Ns. We
-  ;; ask y only for lengths that are remainders after an x-match in
-  ;; 0..max(Ns). (And we call neither x nor y if there are no Ns.)
+(to ((then r s) Ns)
+  ;; Return the set of matches to rs whose total length is in Ns. We
+  ;; ask s only for lengths that are remainders after an r-match in
+  ;; 0..max(Ns). (And we call neither r nor s if there are no Ns.)
   (case (Ns.empty? none)
         (else
-         (let x-matches (x (call set<- (range<- (+ (call max Ns) 1)))))
-         (let Ns-x (call set<- (each '.count x-matches)))
-         (let Ns-y (call set<- (for gather ((n Ns.keys))
-                                 (for gather ((m Ns-x.keys))
+         (let r-matches (r (call set<- (range<- (+ (call max Ns) 1)))))
+         (let Ns-r (call set<- (each '.count r-matches)))
+         (let Ns-s (call set<- (for gather ((n Ns.keys))
+                                 (for gather ((m Ns-r.keys))
                                    (let r (- n m))
                                    (if (< r 0) '() `(,r))))))
-         (let y-matches (y Ns-y))
-         (call set<- (for gather ((m1 x-matches))
-                       (for gather ((m2 y-matches))
+         (let s-matches (s Ns-s))
+         (call set<- (for gather ((m1 r-matches))
+                       (for gather ((m2 s-matches))
                          (if (Ns .maps? (+ m1.count m2.count))
                              `(,(chain m1 m2))
                              '())))))))
 
 (let dot     (one-of "?"))
 (let empty   (literal ""))
-(to (plus x) (then x (star x)))
+(to (plus r) (then r (star r)))
 
 (export
   empty literal then either star plus maybe
