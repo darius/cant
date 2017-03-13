9670de82454f3fa1400e1d8e728dcf721fd54eca
Darius Bacon <darius@wry.me>
2017-03-13 10:30:07 -0700

    shorter cycle-write

--- later/cycle-write.scm
+++ later/cycle-write.scm
@@ -6,65 +6,53 @@
 ;; #1=<box #1>
 
 ;; XXX just a sketch, untested too
-;; XXX what about .display?
+;; TODO exclude known-to-be-acyclic types from the occurs table
 
 (make cycle-write
   ((thing)
    (cycle-write thing out))
   ((thing sink)
-   (let occurs (map<-))
-   (let cyclic? (box<- #no))
+   (let tags (map<-))
    (let buffer (fillvector<-))
-   (let straw-sink (straw-sink<- occurs cyclic? buffer)) ;XXX better name?
-   (straw-sink .write thing)
-   (case (cyclic?.^
-          (let cycle-sink (cycle-sink<- occurs sink))
-          (cycle-sink .write thing))
-         (else
-          (flush buffer sink)))))
+   (let cycle-sink (cycle-sink<- tags buffer)) ;XXX better name?
+   (cycle-sink .write thing)
+   (for each! ((writer buffer.values))
+     (writer sink))))
 
-(to (flush buffer sink)
-  (for each! ((str buffer.values))
-    (sink .display str)))
+;; The tags table keeps a tag for each object the cycle sink
+;; visits. The tag is 0 after the first visit; then, on the second and
+;; thereafter, a positive integer identifying the object.
 
-;; The occurs table gives a value for each object the straw sink is
-;; asked to write. The value is 0 if asked only once, else a positive
-;; id if not yet visited in the 'real' writing pass, else negative.
+;; The buffer accumulates a sequence of procedures to send the final
+;; formatted text to the destination sink.
 
-(to (straw-sink<- occurs cyclic? buffer)
+(to (cycle-sink<- tags buffer)
   (let counter (box<- 0))
-  (make straw-sink
-    ({.display str}                     ;XXX I guess?
-     (buffer .push! str))
-    ({.write thing}
-     (let id (occurs .get thing #no))
-     (case ((not id)
-            (occurs .set! thing 0)
-            (thing .selfie straw-sink))
-           ((= id 0)
-            (counter .^= (+ counter.^ 1)) ;TODO (incr counter) ?
-            (occurs .set! thing counter.^)
-            (cyclic .^= #yes))
-           (else
-            'ok)))
-    ;; XXX ...
-    ))
-
-(to (cycle-sink<- occurs sink)
   (make cycle-sink
-    ({.display str}                     ;XXX I guess?
-     (sink .display str))
-    ({.write thing}
-     (let id (occurs thing))
-     (case ((= id 0)
-            (thing .selfie cycle-sink))
-           ((< 0 id)
-            (occurs .set! thing (- id))
-            (sink .display ("#~w=" .format id))
+    ({.display str}                     ;XXX I guess? Is this the sink protocol?
+     (buffer .push! (given (sink) (sink .display str)))
+    ({.write thing}                     ;XXX or should this be .print?
+     (let tag (tags .get thing #no))
+     (case ((not tag)
+            ;; First visit.
+            (tags .set! thing 0)
+            (buffer .push! (given (sink)
+                             (let id (tags thing))
+                             (when (< 0 id)
+                               (format .to sink "#~w=" id))))
             (thing .selfie cycle-sink))
            (else
-            (sink .display ("#~w" .format (- id))))))
-    ;; XXX ...
-    ))
+            (let id (case ((= tag 0)
+                           ;; Second visit.
+                           (counter .^= (+ counter.^ 1)) ;TODO (incr counter) ?
+                           (tags .set! thing counter.^)
+                           counter.^)
+                          (else
+                           ;; Thereafter.
+                           tag)))
+            (buffer .push! (given (sink)
+                             (format .to sink "#~w" id))))))
+    ;; XXX .print?
+    )))
 
 (export cycle-write)
