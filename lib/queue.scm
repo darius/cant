a55e0c036cb9d63af21161d0a322eca6122b1b01
Darius Bacon <darius@wry.me>
2017-03-13 12:18:26 -0700

    add queue

---
+++ lib/queue.scm
@@ -0,0 +1,32 @@
+;; Functional queues.
+;; Sequences with first-in-first-out addition and removal.
+
+;; The pair {queue left right} represents the sequence (chain left
+;; (reverse right)). Elements are removed on the left and added on the
+;; right. The representation is split this way for amortized efficiency.
+
+(let empty {queue '() '()})
+
+(to (empty? {queue h t})
+  (and h.empty? t.empty?))
+
+(to (push {queue h t} element)
+  {queue h `(,element ,@t)})
+
+(to (extend {queue h t} elements)
+  {queue h `(,@(reverse elements) ,@t)}) ;TODO chain-reverse
+
+;; Return two values, `(,head ,rest-of-queue).
+(to (dequeue {queue h t})               ;TODO rename to pop-front or something?
+  (case (h.empty?
+         (when t.empty?
+           (error "Dequeue of empty queue"))
+         (let seq (reverse t))
+         `(,seq.first {queue ,seq.rest ()}))
+        (else
+         `(,h.first {queue ,h.rest ,t}))))
+
+(export
+  empty empty?
+  push extend
+  dequeue)
