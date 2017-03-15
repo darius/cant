d7b399192740d123773dd2c5536468cfc0f2166a
Darius Bacon <darius@wry.me>
2017-03-15 07:25:23 -0700

    tweak

--- loadme.scm
+++ loadme.scm
@@ -14,11 +14,10 @@
       (import (use "lib/sort")       sort)
       (import (use "lib/hashset")    set<-)
       (import (use "lib/squeam-ast") unparse-exp unparse-pat unparse-clause)
-      ))
-(squeam-interpret
- '(hide ;; Let's default to traceback-on-error.
-   (import (use "lib/traceback") on-error-traceback)
-   (the-signal-handler .^= on-error-traceback)))
+;      (to (unparse-exp e) e) (to (unparse-pat p) p) (to (unparse-clause p) p)
+      (hide ;; Let's default to traceback-on-error.
+        (import (use "lib/traceback") on-error-traceback)
+        (the-signal-handler .^= on-error-traceback))))
 
 (define (repl)
   (squeam-interpret '(repl)))
