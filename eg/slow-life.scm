68ae3b62bd7ee46145dc7c69bdcb2d3ba68e19ab
Darius Bacon <darius@wry.me>
2017-03-17 08:19:58 -0700

    hide cursor; start torusing

--- eg/slow-life.scm
+++ eg/slow-life.scm
@@ -3,7 +3,7 @@
 (import (use "lib/sturm")
   cbreak-mode)
 (import (use "later/ansi-term")
-  home clear-screen)
+  home clear-screen cursor-show cursor-hide)
 
 (to (main (prog @args))
   (let n-steps (match args
@@ -15,12 +15,14 @@
                       "** "
                       " * "))
   (for cbreak-mode ()
+    (display cursor-hide)
     (begin running ((grid grid) (step 0))
       (display clear-screen)
       (display home)
       (show grid)
       (when (< step n-steps)
-        (running grid.next (+ step 1))))))
+        (running grid.next (+ step 1))))
+    (display cursor-show)))
 
 (to (paint grid top left lines)
   (for each! (((i line) lines.items))
@@ -37,10 +39,8 @@
 
 (to (grid<- n-rows n-cols)
 
-  ;; G is an array storing, for each Life grid cell, its value and
-  ;; live-neighbor-count. It has two extra rows and columns for 
-  ;; the edges. The value is the low bit; the remaining bits hold
-  ;; the live neighbor count.
+  ;; G is an array storing, for each Life grid cell, its value (0 or
+  ;; 1). It has two extra rows and columns for the edges.
   (let R (+ n-rows 2))
   (let C (+ n-cols 2))
   (let G (vector<-count (* R C) 0))
@@ -49,23 +49,32 @@
   (let S C)
   (let W (- 1))
   (let E 1)
-  (let NW (+ N W))
-  (let NE (+ N E))
-  (let SW (+ S W))
-  (let SE (+ S E))
+  (let neighbor-dirs
+    `(,(+ N W) ,N ,(+ N E)
+      ,W          ,E
+      ,(+ S W) ,S ,(+ S E)))
 
   ;; r in [1..n-rows], c in [1..n-cols].
   (to (at r c)
     (+ (* C r) c))
 
   (to (update i)
-    (match (sum (for each ((d `(,NW ,N ,NE
-                                 ,W     ,E
-                                ,SW ,S ,SE)))
-                  (G (+ i d))))
+    (match (sum (for each ((dir neighbor-dirs))
+                  (G (+ i dir))))
       (2 (G i))
       (3 1)
       (_ 0)))
+
+  ;; Make the world toroidal by copying the edges of the array so that
+  ;; row #1 effectively neighbors row #n-rows, and likewise for the
+  ;; columns.
+  ;; XXX wrong on top edge, at least
+  (to (copy-edges)
+    (for each! ((r (range<- 1 (+ n-rows 1))))
+      (G .set! (at r 0)       (G (at r n-cols)))
+      (G .set! (at r (- C 1)) (G (at r 1))))
+    (G .copy! (at 0 0)       (at n-rows 0) C)
+    (G .copy! (at (- R 1) 0) (at 1 0)      C))
 
   (make life-grid
     ((r c)
@@ -78,7 +87,7 @@
        (for each ((c (range<- 1 (+ n-cols 1))))
          (G (at r c)))))
     ({.next}
-     ;; TODO: toroidal world
+     (copy-edges)
      (let new (grid<- n-rows n-cols))
      (for each ((r (range<- 1 (+ n-rows 1))))
        (for each ((c (range<- 1 (+ n-cols 1))))
