1828acd600e7bd7dfc99d88c9c1f72f6f9ea4e55
Darius Bacon <darius@wry.me>
2017-03-17 07:40:39 -0700

    get ansi-term to at least load

--- later/ansi-term.scm
+++ later/ansi-term.scm
@@ -3,20 +3,20 @@
 
 (let prefix (string<- (char<- 27) #\[)) ;TODO more string escapes
 
-(define (seq string) (chain prefix string))
+(to (seq string) (chain prefix string))
 
 (let home               (seq "H"))
 (let clear-to-bottom    (seq "J"))
-(let clear-screen       (seq "2J" home))
+(let clear-screen       (chain prefix "2J" home))
 (let clear-to-eol       (seq "K"))
 
 (let save-cursor-pos    (seq "s"))
 (let restore-cursor-pos (seq "u"))
 
-(let show-cursor        (seq (string<- (char<- 25) #\h)))
-(let hide-cursor        (seq (string<- (char<- 25) #\l)))
+(let cursor-show        (seq "?25h"))
+(let cursor-hide        (seq "?25l"))
 
-(define (goto x y)
+(to (goto x y)
   (chain prefix
          (string<-number (+ y 1)) ";"
          (string<-number (+ x 1)) "H"))
@@ -24,14 +24,14 @@
 (let (black red green yellow blue magenta cyan white)
   (chain (range<- 8) '()))              ;TODO ugly
 
-(define (bright color)
+(to (bright color)
   (+ 60 color))
 
-(define (sgr num)
+(to (sgr num)
   (seq (string<-number num) "m")) ;TODO format to string
 
-(define (set-foreground color) (sgr (+ 30 color)))
-(define (set-background color) (sgr (+ 40 color)))
+(to (set-foreground color) (sgr (+ 30 color)))
+(to (set-background color) (sgr (+ 40 color)))
 
 (export
   home
@@ -40,8 +40,8 @@
   clear-to-eol
   save-cursor-pos
   restore-cursor-pos
-  show-cursor
-  hide-cursor
+  cursor-show
+  cursor-hide
   goto 
   black red green yellow blue magenta cyan white
   bright
