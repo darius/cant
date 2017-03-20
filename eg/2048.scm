1ad8595676919973b2dcbbe78b6c3c0750e56244
Darius Bacon <darius@wry.me>
2017-03-19 20:45:36 -0700

    start on 2048

---
+++ eg/2048.scm
@@ -0,0 +1,117 @@
+;; Ported from github.com/darius/sturm
+
+(import (use "lib/sturm")
+  cbreak-mode
+  get-key
+  render)
+
+(to (main _)
+  (for cbreak-mode ()
+    (play (starting-board<-))))
+
+(to (play board)
+  (let history (fillvector<-))
+  (begin playing ((board board) (forfeit? #no))
+    (to (continue)
+      (playing board forfeit?))
+    (let score (case ((lost? board) "You lose!")
+                     (forfeit?      "You forfeit.")
+                     ((won? board)  "You win!")
+                     (else          "")))
+    (frame board score)
+    (let key (get-key))
+    (case (("Qq" .find? key) 'quitting)
+          (("Uu" .find? key)
+           (if history.empty?
+               (playing history.pop! #yes)
+               (continue)))
+          ((arrows .maps? key)
+           (let sliding ((arrows key) board))
+           (case (sliding.empty? (continue))
+                 (else
+                  (history .push! board)
+                  (animate sliding score)
+                  (let next-board (plop sliding.last
+                                        (if (< (random-integer 10) 9) 2 4)))
+                  (playing next-board forfeit?))))
+          (else
+           (continue)))))
+
+(let heading "Use the arrow keys, U to undo (and forfeit), or Q to quit.\n\n")
+
+(to (frame board score)
+  (render `(,heading ,(view board) ,score)))
+
+(to (animate boards score)
+  (for each! ((board boards))
+    (frame board score)
+    ; XXX (sleep 0.1)
+    ))
+
+(to (starting-board<-)
+  (plop (plop empty-board 2) 2))
+
+(to (plop board value)
+  (update board (random-empty-square board) value))
+
+;; XXX this object doesn't really buy us anything
+;; we could just use the list this time
+
+
+(to (view rows)
+  (for each ((row rows))
+    `(,(for each ((v row))
+         (" ~d" .format v))          ;TODO justify and style
+      "\n\n")))
+
+(to (won? rows)
+  (for some ((row rows))
+    (for some ((v row))
+      (<= 2048 v))))
+
+(to (lost? rows)
+  (for every ((move arrows.values))
+    ((move board) .empty?)))         ;XXX or #no?
+
+(to (random-empty-square rows)
+  (random-choice (for gather (((r row) rows.items))
+                   (for filter (((c v) row.items))
+                     (and (= v 0) `(,r ,c))))))
+
+(to (random-choice xs)       ;XXX should be in lib or standard methods
+  (xs (random-integer xs.count)))
+
+(to (update rows at new-value)
+  (for each (((r row) rows.items))
+    (for each (((c v) row.items))
+      (if (= at `(,r ,c)) new-value v))))
+
+(to (left rows)
+  (begin sliding ((states (for each ((row rows))
+                            (slide 0 row))))
+    (if (for every ((lo states.values))
+          ...)
+        ...
+        ...)))
+
+;; Slide row one place leftward, leaving fixed any places left of lo.
+;; Advance lo past merging or completion.
+(to (slide lo row)
+  XXX)
+
+(to (right rows) (each flip-h (left (flip-h rows))))
+(to (up rows)    (each flip-d (left (flip-d rows))))
+(to (down rows)  (each flip-d (right (flip-d rows))))
+
+(to (flip-d rows)
+  xxx)
+
+(to (flip-h rows)
+  xxx)
+
+(let arrows (export left right up down))
+
+(let empty-board '((0 0 0 0)   ;XXX crude
+                   (0 0 0 0)
+                   (0 0 0 0)
+                   (0 0 0 0)))
