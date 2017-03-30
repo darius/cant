2b48c83579becb4caad8619f6a9941f389652e4c
Darius Bacon <darius@wry.me>
2017-03-29 17:10:01 -0700

    adapt prettyprinter for strictness (not quite all the way)

--- lib/pretty-layout.scm
+++ lib/pretty-layout.scm
@@ -1,82 +1,82 @@
 ;; Pretty printing
-;; Let's start by trying out Wadler's "A prettier printer".
-;; TODO more efficient (with laziness, etc.)
+;; From Wadler's "A prettier printer", adapted for strict evaluation.
 
 ;; Document datatypes:
-;; ()                 Empty document
-;; {text s doc}       String s followed by doc
-;; {line i doc}       (Newline, then i spaces) or 1 space if flattened; then doc
-;; {union doc1 doc2}  Any layout described by either doc1 or doc2.
-;;                    Invariant: (doc1, doc2) are equal after flattening,
-;;                               and doc2 is "no flatter".
+
+;; (@docs)       Concatenation of docs
+;; {text s}      Literal string
+;; {line i}      (Newline, then i spaces) or 1 space if flattened
+;; {union x y}   Any layout described by either of docs x or y.
+;;               Invariant: (x, y) are equal after flattening,
+;;                          and y is "no flatter".
+
+;; TODO really because of strictness we should make {group x} a
+;; basic doc type in place of {union x y}.
 
 (let nil       '())
-(to (text str) {text str '()})
-(let line      {line 0 '()})
-(to (<> @docs) (foldr <>2 docs '()))
-
-(to (<>2 y z)
-  (match y
-    (()          z)
-    ({text s x}  (text<> s (<>2 x z)))
-    ({line i x}  {line i (<>2 x z)})
-    ({union w x} {union (<>2 w z) (<>2 x z)})))
-
-(to (text<> s x) ; Just a performance hack for {text s x}; maybe not worthwhile.
-  (match x
-    ({text t y} {text (chain s t) y})
-    (_          {text s x})))
+(to (text str) {text str})
+(let line      {line 0})
+(make <>
+  ((doc) doc)
+  ((@docs) docs))
 
 (to (nest i doc)
   (match doc
-    (()          '())
-    ({text s x}  {text s (nest i x)})
-    ({line j x}  {line (+ i j) (nest i x)})
-    ({union x y} {union (nest i x) (nest i y)})))
+    ({text s}    doc)
+    ({line j}    {line (+ i j)})
+    ({union x y} {union (nest i x) (nest i y)})
+    ((@docs)     (for each ((d docs))
+                   (nest i d)))))
 
 (to (group doc)
-  (match doc
-    (()          '())
-    ({text s x}  {text s (group x)})
-    ({line _ _}  {union (flatten doc) doc})
-    ({union x y} {union (group x) y})))
+  (let flat (flatten doc)) ;XXX this needs to happen in (best) instead, lazily
+  (if (= flat doc) doc {union flat doc}))
 
 (to (flatten doc)
   (match doc
-    (()          '())
-    ({text s x}  (text<> s (flatten x)))
-    ({line _ x}  (text<> " " (flatten x)))
-    ({union x _} (flatten x))))
+    ({text s}    doc)
+    ({line i}    {text " "})
+    ({union x y} (flatten x))
+    ((@docs)     (each flatten docs))))
 
-;; Formatting
+(to (pretty-print doc width)
+  (lay-out width (best width 0 doc)))
 
-(to (pretty-print doc @opt-width)
-  (let width (match opt-width (() 72) ((w) w)))
-  (lay-out (best width 0 doc)))
-
+;; Return a 'semidetermined' doc: one with no {union...} in the
+;; first line, and where {text...} and {line...} appear only in a
+;; (@docs) list (which may have no other types of elements).
 (to (best w c doc)
   (match doc
-    (()          '())
-    ({text s x}  {text s (best w (+ c s.count) x)})
-    ({line i x}  {line i (best w i x)})
+    ({text _}      `(,doc))
+    ({line _}      `(,doc))
+    (()            doc)
+    (((@x) @y)     (best w c (chain x y))) ;TODO chain in <> instead?
+    (({line _} @_) doc)
+    (({text s} @x) `({text ,s} ,@(best w (+ c s.count) x)))
+    (({union x y} @z)
+     (best w c {union `(,x ,@z) `(,y ,@z)}))
     ({union x y}
      (let best-x (best w c x))
      (if (fits? (- w c) best-x)
          best-x
          (best w c y)))))
 
-(to (fits? w doc)
-  (and (<= 0 w) (match doc
-                  ({text s x} (fits? (- w s.count) x))
+(to (fits? w semidetermined-doc)
+  (and (<= 0 w) (match semidetermined-doc
+                  (({text s} @x) (fits? (- w s.count) x))
                   (_ #yes))))
 
-(to (lay-out doc)
-  (match doc
-    (()         "")
-    ({text s x} (chain s (lay-out x)))
-    ({line i x} (chain "\n" (" " .repeat i) (lay-out x)))))
+(to (lay-out w semidetermined-doc)
+  (match semidetermined-doc
+    (() "")
+    (({line i} @x)
+     (chain "\n"
+            (" " .repeat i)
+            (lay-out w (best w i x))))
+    (({text s} @x)
+     (chain s (lay-out w x)))))
 
 
 (export
-  pretty-print lay-out
+  pretty-print lay-out best
   nil <> text line nest group)
