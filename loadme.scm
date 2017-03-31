53bfbec7e2c7ec6203ac427a7a8ccdce0dee50fb
Darius Bacon <darius@wry.me>
2017-03-31 15:26:24 -0700

    load unparse-foo first, because needed for traceback

--- loadme.scm
+++ loadme.scm
@@ -8,13 +8,15 @@
 
 (run-load "lib/stdlib.scm")
 (squeam-interpret
- '(do (import (use "lib/format")     format)
-      (import (use "lib/fillvector") fillvector<-)
-      (import (use "lib/sort")       sort)
-      (import (use "lib/hashset")    set<-)
-      (import (use "lib/squeam-ast") unparse-exp unparse-pat unparse-clause)
-      ;; Let's default to traceback-on-error:
-      (the-signal-handler .^= ((use "lib/traceback") 'on-error-traceback))))
+ '(do
+    ;; unparse-foo are used by runtime for tracebacks:
+    (import (use "lib/squeam-ast") unparse-exp unparse-pat unparse-clause)
+    (import (use "lib/format")     format)
+    (import (use "lib/fillvector") fillvector<-)
+    (import (use "lib/sort")       sort)
+    (import (use "lib/hashset")    set<-)
+    ;; Let's default to traceback-on-error:
+    (the-signal-handler .^= ((use "lib/traceback") 'on-error-traceback))))
 
 (define (repl)
   (squeam-interpret '(repl)))
