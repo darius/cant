489614c7a97c0465410270b44e5c8be483086db3
Darius Bacon <darius@wry.me>
2017-03-31 09:53:58 -0700

    typing drill

---
+++ eg/typing-drill.scm
@@ -0,0 +1,40 @@
+;; How fast do you type?
+
+(import (use "lib/sturm") cbreak-mode render cursor get-key)
+
+(to (main _)
+  (cbreak-mode interact))
+
+(to (interact)
+  (show 0 "(Start typing...)")
+  (let strokes (fillvector<- (get-key)))
+  (let start (nano-now))
+  (begin typing ()
+    (show (/ (- (nano-now) start) 1000000000)
+          (string<-list (as-list strokes))) ;XXX ugly
+    (case (stdin.ready?
+           (match (get-key)
+             ('esc
+              'done)
+             ('backspace
+              (unless strokes.empty?
+                strokes.pop!)
+              (typing))
+             (key
+              (when (char? key)
+                (strokes .push! key))
+              (typing))))
+          (else
+           ;; XXX This polling/sleeping approach sucks, but it's
+           ;; supported by the underlying Scheme.
+           (nanosleep 50000000)  ; 1/20 sec
+           (typing)))))
+
+(to (show t body)
+  (let cps (if (or (= t 0) body.empty?)
+               0
+               (/ (- body.count 1) t)))
+  (render `(,("~w seconds  ~w words/minute"
+              .format t (* cps 60 (/ 1 5)))
+            "   (Hit Esc to quit.)\n\n"
+            ,body ,cursor)))
