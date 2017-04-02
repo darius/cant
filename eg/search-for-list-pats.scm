04fa85aec470926227060abe4d70e5d49614a6ef
Darius Bacon <darius@wry.me>
2017-04-01 20:17:22 -0700

    search for list pats

---
+++ eg/search-for-list-pats.scm
@@ -0,0 +1,29 @@
+;; OK, let's get an idea what search-squeam-code should look like,
+;; from an actual application: finding instances of the pattern syntax
+;; I'm about to outlaw. I.e., patterns like (a b) instead of `(,a ,b).
+
+(import (use "lib/pretty-print") pp)
+(import (use "lib/squeam-source-walker") expr-subparts patt-subparts)
+
+(to (main `(,_ ,@filenames))
+  (each! report-badness filenames))
+
+(to (report-badness filename)
+  (match (those bad-expr? (with-input-file read-all filename))
+    ('() 'ok)
+    (top-level-bad-exprs
+     (format "In ~d these top-level expressions contain badness:\n" filename)
+     (each! pp top-level-bad-exprs)
+     (newline))))
+
+(to (bad-expr? expr)
+  (bad-part? (expr-subparts expr)))
+
+(to (bad-part? `(,subexprs ,subpatts))
+  (or (some bad-expr? subexprs)
+      (some bad-patt? subpatts)))
+
+(to (bad-patt? patt)
+  (or (and (cons? patt)
+           (not ('(quote quasiquote : @ optional) .find? patt.first)))
+      (bad-part? (patt-subparts expr))))
