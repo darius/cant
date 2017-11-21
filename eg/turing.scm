586f42029151c0498066ee61edd8856866459e86
Darius Bacon <darius@wry.me>
2017-11-20 20:53:17 -0800

    add turing

---
+++ eg/turing.scm
@@ -0,0 +1,55 @@
+;; Turing machine interpreter
+;; XXX untested
+
+;; state = function from mark to `(,actions ,state-id).
+;; action = '< | '> | mark
+
+(to (turer<- states start-state-id)
+  (let state-id (box<- start-state-id))
+  (let tape (box<- '(() - ()))) ;; `(,ls ,head ,rs) where:
+  ;; ls   = reverse of the list of tape squares to the left of the tape head.
+  ;; head = the value of the square under the tape head.
+  ;; rs   = the squares to the right of the tape head.
+  ;; The lists ls and rs only extend so far as has been visited up to now.
+  ;; Visited blank squares are symbolized as '-'.
+
+  (make turer
+
+    ({set-tape! marks}
+     (let `(,h ,@R) (peek marks))
+     (tape .^= `(() ,h ,R)))
+
+    ({run} (turer .run #no))
+    ({run option}
+     (begin running ()
+       (match option
+         ('loudly (print turer.show-config)))
+       (when (states .maps? state-id.^)
+         turer.step
+         (running))))
+
+    ({step}
+     (let `(,_ ,h ,_) tape.^)
+     (let `(,acts ,next-state-id) ((states state-id.^) h))
+     (tape .^= (for foldl ((t tape.^) (act acts))
+                 (let `(,L ,h ,R) t)
+                 (match act
+                   ('< (let `(,l0 ,@ls) (peek L))
+                       `(,ls ,l0 (,h ,@R)))
+                   ('> (let `(,r0 ,@rs) (peek R))
+                       `((,h ,@L) ,r0 ,rs))
+                   (mark `(,L ,mark ,R)))))
+     (state-id .^= next-state-id))
+
+    ({show-config}
+     (let `(,L ,h ,R) tape.^)
+     `(,@(reverse L) (,state-id.^ h) ,@R))
+    ))
+
+(to (peek marks)
+  (match marks
+    ('() '(-))
+    (_ marks)))
+
+;; TODO a canonicalizer that rewrites to an equiv turing machine
+;; that steps one square at a time.
