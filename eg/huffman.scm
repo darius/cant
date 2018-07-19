3c62cecaa5bc5dff0a51a4f2f61c02c3b34aa843
darius <withal@gmail.com>
2018-07-18 23:07:30 -0400

    tweak

--- eg/huffman.scm
+++ eg/huffman.scm
@@ -7,10 +7,10 @@
   empty-pq unit-pq pq-merge pq-insert pq-remove-min)
 
 (to (build-tree freqs)
-  (let leaves (for each ((`(,frequency ,symbol) freqs))
-                `(,frequency {leaf ,symbol})))
+  (let pqs (for each ((`(,frequency ,symbol) freqs))
+             (unit-pq `(,frequency {leaf ,symbol}))))
   ;; TODO? define pq-merge-many or something
-  (begin building ((pq (foldr pq-merge (each unit-pq leaves) empty-pq)))
+  (begin building ((pq (foldr pq-merge pqs empty-pq)))
     ;; TODO? define pq-dequeue or something
     (let `(,f1 ,t1) (pq-min pq))
     (let rest (pq-remove-min pq))
