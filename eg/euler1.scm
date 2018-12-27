7579995c7074c23e456af8b01e3258db2c6fd81d
darius <withal@gmail.com>
2018-12-27 08:29:12 -0800

    tweak

--- eg/euler1.scm
+++ eg/euler1.scm
@@ -2,11 +2,10 @@
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
 (to (euler1 n)
-  (sum (for each ((i (3 .up-to< n)))
-         (if (or (= 0 (i .remainder 3))
-                 (= 0 (i .remainder 5)))
-             i
-             0))))
+  (sum (for filter ((i (3 .up-to< n)))
+         (and (or (= 0 (i .remainder 3))
+                  (= 0 (i .remainder 5)))
+              i))))
 
 ;(print (euler1 10))
 (print (euler1 200))
