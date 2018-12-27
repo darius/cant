f5babad6d1cb395a30ed85a5b358f515ee406f2e
darius <withal@gmail.com>
2018-12-26 21:39:35 -0800

    extract factor.scm

--- eg/euler3.scm
+++ eg/euler3.scm
@@ -1,15 +1,10 @@
 ;; https://projecteuler.net/problem=3
 ;; The largest prime factor of 600851475143.
 
-(to (euler3 n)
-  (let factors (factor n))
-  (factors (- factors.count 1)))
+(import (use "lib/factor") factor)
 
-(to (factor n)
-  (begin trying ((d 2) (n n))
-    (if (= 0 (n .remainder d))
-        (cons d (if (= d n) '() (trying d (n .quotient d))))
-        (trying (+ d 1) n))))
+(to (euler3 n)
+  (call max (factor n)))
 
 ;(print (euler3 13195))
 (print (euler3 131957))
---
+++ lib/factor.scm
@@ -0,0 +1,14 @@
+;; Return a list of the prime factors of a positive integer.
+;; TODO speed
+
+(to (factor n)
+  (surely (integer? n))
+  (surely (< 0 n))
+  (if (= n 1)
+      '()
+      (begin trying ((d 2) (n n))
+        (if (= 0 (n .remainder d))
+            (cons d (if (= d n) '() (trying d (n .quotient d))))
+            (trying (+ d 1) n)))))
+
+(export factor)
