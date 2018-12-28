4eaab1e1470a75bf92bcca0147d46c814fa9ba1a
darius <withal@gmail.com>
2018-12-27 21:09:03 -0800

    add infix.scm

---
+++ eg/infix.scm
@@ -0,0 +1,46 @@
+;; Format an infix expression into a string with minimal parens.
+
+(to (format-infix e)
+  (fmt e 0))
+
+(to (fmt e p)
+  (match e
+    ((? number?)
+     (string<-number e))
+    ((? symbol?)
+     e.name)
+    (`(,op ,x)
+     (match (unaries .get op)
+       (#no
+        ("~w(~d)" .format op (fmt x 0)))
+       (`(,prefix ,postfix)
+        (case (prefix  (enclose prefix p ("~w~d" .format op (fmt x prefix))))
+              (postfix (enclose prefix p ("~d~w" .format (fmt x prefix) op)))))))
+    (`(,op ,x ,y)
+     (match (binaries .get op)
+       (#no
+        ("~w(~d, ~d)" .format op (fmt x 0) (fmt y 0)))
+       (`(,left ,right)
+        (enclose left p
+                 ("~d ~w ~d" .format (fmt x left) op (fmt y right))))))))
+
+(to (enclose inner outer string)
+   (if (< inner outer)
+       ("(~d)" .format string)
+       string))
+
+(let unaries (map<-))
+(let binaries (map<-))
+
+(to (def-prefix name p)  (unaries .set! name `(,p #no)))
+(to (def-postfix name p) (unaries .set! name `(#no ,p)))
+
+(to (def-infix name p)   (binaries .set! name `(,p ,(+ p 1))))
+
+(def-prefix '- 100)                     ;XXX dunno
+
+(def-infix '+ 10)
+(def-infix '- 10)
+(def-infix '* 20)
+(def-infix '/ 20)
+(binaries .set! '^ '(30 30))   ; right-to-left associativity (is this correct?)
