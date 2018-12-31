482f251ff4bf45c8416c3b7c426c3e85d3ba4e60
darius <withal@gmail.com>
2018-12-30 18:44:17 -0800

    oops, missing squeam.scm!

---
+++ squeam.scm
@@ -0,0 +1,31 @@
+#! /usr/bin/scheme --program
+#!chezscheme
+(import (chezscheme)
+  (terp util)
+  (terp macros)
+  (terp read)
+  (terp parse)
+  (terp env)
+  (terp elaborate)
+  (terp primitives)
+  (terp terp))
+
+;; The Squeam interpreter and global environment.
+
+(run-load "lib/stdlib.scm")
+(squeam-interpret
+ '(do
+;    (use "test/smoke-test")
+    ;; Let's default to traceback-on-error:
+    ;; TODO: also stash the error in the-last-error for below
+;    (push-signal-handler ((use "lib/traceback") 'on-error-traceback))
+    (import (use "lib/flexarray")  flexarray<-)
+    (import (use "lib/sort")       sort sort-by)
+    (import (use "lib/bag")        bag<-)))
+
+(define (repl . opt-args)
+  (squeam-interpret `(call repl ',opt-args)))
+
+;(unless (squeam-interpret '(the-last-error .^))
+(repl (cdr (command-line)))
+;)
