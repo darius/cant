6d4f52cf7801d03eedbfc01527b30f659f73b4a0
darius <withal@gmail.com>
2019-01-03 22:30:47 -0800

    gc unused

--- run.scm
+++
@@ -1,7 +0,0 @@
-(load "loadme.scm")
-
-(let ((args (command-line)))
-  ;; TODO use load-and-run from stdlib
-  (run-load (cadr args))
-  (if (env-defined? repl-env 'main)
-      (squeam-interpret `(main ',(cdr args)))))
