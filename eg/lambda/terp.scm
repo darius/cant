c7642b05201dbb4bc3e8eb1a70bcf8b91a90cfb8
darius <withal@gmail.com>
2019-06-21 09:59:21 -0700

    oops

--- eg/lambda/terp.scm
+++ eg/lambda/terp.scm
@@ -107,7 +107,7 @@
 
      (to (if pred y n) (pred n y))
      (to (and p q if-no if-yes) (p if-no (q if-no if-yes)))
-     (to (or p q if-no if-yes) (p if-yes (q if-no if-yes)))
+     (to (or p q if-no if-yes) (p (q if-no if-yes) if-yes))
 
      (to (zero f x) x)
      (to (succ n f x) (f (n f x)))
