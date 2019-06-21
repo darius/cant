314d116a08189034c41606b5bf4e6b24ee677d69
darius <withal@gmail.com>
2019-06-20 20:27:00 -0700

    factor out prelude; church<-count

--- eg/lambda/terp.scm
+++ eg/lambda/terp.scm
@@ -2,6 +2,7 @@
 
 (to (seq-parse lexps)
   (match lexps
+    ('((the-environment))  {the-env})   ;TODO not meant as part of the actual language
     (`(,e)                 (exp-parse e))
     (`((let ,p ,e) ,@es)   (exp-parse `(([,p] ,@es) ,e)))
     (`((to ,@_) ,@_)       (def-parse lexps.first lexps.rest))
@@ -38,14 +39,15 @@
 ;; Interpreter
 
 (to (terp e @(optional r))
-  (ev (exp-parse e) (or r {builtins})))
+  (ev (exp-parse e) (or r prelude-env)))
 
 (to (ev e r)
   (match e
     ({const c} c)
     ({var v}   (lookup r v))
     ({lam _ _} {closure e r})
-    ({app f a} (apply (ev f r) (ev a r)))))
+    ({app f a} (apply (ev f r) (ev a r)))
+    ({the-env} r)))
 
 (to (apply fn val)
   (match fn
@@ -65,10 +67,38 @@
     ({builtins}
      (builtins v))))
 
+(to (church<-count n)
+  (let zero (lookup prelude-env 'zero))
+  (let succ (lookup prelude-env 'succ))
+  (begin counting ((n n))
+    (match n
+      (0 zero)
+      ((? count?) (apply succ (counting (- n 1)))))))
+
 (let builtins
   (map<- `(
            (add1 {primitive ,(given (n) (+ n 1))})
+           (church<-count {primitive ,church<-count})
            )))
+
+(let prelude
+  '(do 
+
+     (let no  ([p x y] (p x)))
+     (let yes ([p x y] (p y)))
+     (to (if pred y n) (pred n y))
+
+     (to (zero f x) x)
+     (to (succ n f x) (f (n f x)))
+     (to (count<-church n) (n add1 0))
+
+     (to (+ m n f x) (m f (n f x)))
+     (to (* m n f x) (m (n f) x))
+     (to (expt m n) (n m))
+     
+     (the-environment)))
+
+(let prelude-env (terp prelude {builtins}))
 
 
 ;; Main
@@ -78,20 +108,7 @@
 
 (let eg
   '(do 
-
-     (let no  ([p x y] (p x)))
-     (let yes ([p x y] (p y)))
-     (to (if pred y n) (pred n y))
-
-     (to (zero f x) x)
-     (to (succ n f x) (f (n f x)))
-     (to (+ m n f x) (m f (n f x)))
-     (to (* m n f x) (m (n f) x))
-     (to (expt m n) (n m))
-
-     (to (int<-church n) (n add1 0))
-
      (to (one f x) (f x))
      (let two (+ one one))
-     (let three (+ one two))
-     (int<-church (expt two three))))
+     (let three (church<-count 3))
+     (count<-church (expt two three))))
