edeb5a8a97d584cfbac6e62f6461aad0a24c4c62
darius <withal@gmail.com>
2019-07-12 20:40:30 -0700

    use bounded sink in traceback printing

--- lib/traceback.scm
+++ lib/traceback.scm
@@ -3,8 +3,6 @@
 ;; We try to call on less of the library than usual, to work better
 ;; when the system is borked.
 
-(import (use "lib/cycle-write") cycle-write)
-
 (to (on-error-traceback k @evil)
   (display "Error! Traceback:\n")
   (print-traceback k)
@@ -18,15 +16,31 @@
   (for each! ((frame (reverse k)))
     (format "  ~d\n" (write-to-bounded-string frame 77))))
 
-(to (write-to-bounded-string frame width)
-  ;; TODO custom sink type, using an ejector to quit early, don't bother about cycles
-  (let s (with-output-string (given (sink)
-                               (cycle-write frame sink))))
-  ;; TODO make it unambiguous whether it's truncated, somehow
-  (if (< s.count width)
-      s
-      (chain (s .slice 0 (- width 2)) "..")))
-
+;; Write `thing` into a string, but give up and truncate if it
+;; overflows `width`.
+(to (write-to-bounded-string thing width)
+  (let buffer (flexarray<-))
+  (to (output)
+    (let s ("" .join buffer))
+    ;; TODO make it unambiguous whether it's truncated, somehow
+    (if (< s.count width)
+        s
+        (chain (s .slice 0 (- width 2)) "..")))
+  (with-ejector
+   (given (ejector)
+     (let ss (string-sink<-))
+     (to (cut-off)
+       (buffer .push! ss.output-string) ;TODO worth checking to skip if output-string empty?
+       (when (<= width (sum (each '.count buffer)))
+         (ejector .eject (output))))
+     (make bounded-sink
+       ({.display a}   (ss .display a)   (cut-off))
+       ({.write-u8 u8} (ss .write-u8 u8) (cut-off))
+       ({.print a}     (ss .print a)     (cut-off))
+       ({.close}       (ss .close)))
+     (bounded-sink .print thing)
+     (output))))
+       
 (to (complain @evil)
   (match evil
     (`(,(? string? plaint) ,@values)
