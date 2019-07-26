606f3b53cc0cda0838a5739880e4338b55a60732
darius <withal@gmail.com>
2019-07-26 09:11:36 -0700

    more test

--- test/test-ssets.scm
+++ test/test-ssets.scm
@@ -2,10 +2,8 @@
   sset<- sset<-list sset-elements sset-insert sset-remove sset-union sset-difference)
 
 (import (use "lib/squickcheck")
-  a-claim a-nat an-int a-char a-printable-char a-printable-string a-list-of a-tuple a-choice
-  weighted-choice
-  all should  ;; I dunno what to call it yet
-  )
+  a-nat a-list-of
+  all should)
 
 (let s1 (sset<- 1 7 5 3))
 (let s2 (sset<- 0 2 3 4 5 9))
@@ -18,4 +16,11 @@
   (let sx (sset<-list x))
   (let mx (set<-list x))
   (should = (sset-elements sx) (sort mx.keys)))
-;; TODO more
+
+(for all ((x (a-list-of a-nat))
+          (y (a-list-of a-nat)))
+  (let sx (sset<-list x)) (let sy (sset<-list y)) (let sr (sset-union sx sy))
+  (let mx (set<-list x))  (let my (set<-list y))  (let mr (mx .union my))
+  (should = (sset-elements sr) (sort mr.keys)))
+
+;; TODO this is getting pretty clumsy
