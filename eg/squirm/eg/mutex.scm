66fb7529fae2817cf3e4cd4fad3e4f69aa041c49
darius <withal@gmail.com>
2019-07-31 10:30:11 -0700

    mutex example

---
+++ eg/squirm/eg/mutex.scm
@@ -0,0 +1,44 @@
+;; exports: start stop wait signal
+
+;; This example is interesting in that the state I'd at first think of
+;; as naturally living in the server processs (i.e. the list of
+;; processes waiting on the mutex) is not represented in the server
+;; loop's state arguments -- it's implicit in the process mailbox
+;; instead.
+
+(to (main)
+  (start)
+  (spawn (given ()
+           (wait)
+           (signal)))
+  (wait)
+  (signal)
+  ;; TODO more
+  (print "done"))
+
+(to (start)
+  (register 'mutex (spawn free)))
+
+(to (stop)
+  (! 'mutex 'stop))
+
+(to (wait)
+  (! 'mutex ['wait (me)])
+  (? ('ok 'ok)))
+
+(to (signal)
+  (! 'mutex ['signal (me)])
+  'ok)
+
+(to (free)
+  (? (['wait pid]
+      (! pid 'ok)
+      (? (['signal (: pid)]
+          (free))))
+     ('stop
+      (begin killing ()
+        (? (['wait pid]
+            (exit pid 'kill)
+            (killing))
+           ((after 0)
+            'ok))))))
