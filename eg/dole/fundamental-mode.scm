6c0426281b2fe399c8123737033ba0f0a16ea934
darius <withal@gmail.com>
2019-08-05 20:27:15 -0700

    reindent

--- eg/dole/fundamental-mode.scm
+++ eg/dole/fundamental-mode.scm
@@ -8,30 +8,30 @@
 (to (fundamental-mode<-)
   (let buffer (buffer<-))
 
-   (to (backward-char _) (buffer .move-char -1))
-   (to (forward-char _)  (buffer .move-char  1))
+  (to (backward-char _) (buffer .move-char -1))
+  (to (forward-char _)  (buffer .move-char  1))
 
-   (let bindings
-     `((,(C #\B)  ,backward-char)
-       (left      ,backward-char)
-       (,(C #\F)  ,forward-char)
-       (right     ,forward-char)
-       (,(C #\N)  ,(-> (buffer .next-line)))
-       (down      ,(-> (buffer .next-line)))
-       (,(C #\P)  ,(-> (buffer .previous-line)))
-       (up        ,(-> (buffer .previous-line)))
-       (,(C #\Q)  exit)
-       (,(C #\M)  ,(-> (buffer .insert "\n")))
-       (backspace ,(-> (buffer .backward-delete-char)))
-       (del       ,(-> (buffer .forward-delete-char)))
-       (end       ,(-> (buffer .end-of-line)))
-       (home      ,(-> (buffer .beginning-of-line)))
-       (pg-up     ,(-> (buffer .previous-page)))
-       (pg-dn     ,(-> (buffer .next-page)))))
+  (let bindings
+    `((,(C #\B)  ,backward-char)
+      (left      ,backward-char)
+      (,(C #\F)  ,forward-char)
+      (right     ,forward-char)
+      (,(C #\N)  ,(-> (buffer .next-line)))
+      (down      ,(-> (buffer .next-line)))
+      (,(C #\P)  ,(-> (buffer .previous-line)))
+      (up        ,(-> (buffer .previous-line)))
+      (,(C #\Q)  exit)
+      (,(C #\M)  ,(-> (buffer .insert "\n")))
+      (backspace ,(-> (buffer .backward-delete-char)))
+      (del       ,(-> (buffer .forward-delete-char)))
+      (end       ,(-> (buffer .end-of-line)))
+      (home      ,(-> (buffer .beginning-of-line)))
+      (pg-up     ,(-> (buffer .previous-page)))
+      (pg-dn     ,(-> (buffer .next-page)))))
 
-   (for each! ((`(,key ,command) bindings)) ;TODO should be a key-map method
-     (buffer.key-map .set! key command))
+  (for each! ((`(,key ,command) bindings)) ;TODO should be a key-map method
+    (buffer.key-map .set! key command))
       
-   buffer)
+  buffer)
 
 (export fundamental-mode<-)
