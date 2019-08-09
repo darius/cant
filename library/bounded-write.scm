8c44c9e146629d4aee72ac4ab5bcb6b6630bea66
darius <withal@gmail.com>
2019-08-08 22:28:42 -0700

    extract bounded-write

---
+++ library/bounded-write.scm
@@ -0,0 +1,28 @@
+;; Write `thing` into a string, but give up and truncate if it
+;; overflows `width`.
+(to (write-to-bounded-string thing width)
+  (let buffer (flexarray<-))
+  (let total (box<- 0)) ;; Kept equal to (sum _.count buffer)
+  (to (output)
+    (let s ("" .join buffer.values))
+    ;; TODO make it unambiguous whether it's truncated, somehow
+    (if (< total.^ width)
+        s
+        (chain (s .slice 0 (- width 2)) "..")))
+  (with-ejector
+   (on (ejector)
+     (let ss (string-sink<-))
+     (to (cut-off)
+       (buffer .push! ss.output-string) ;TODO worth checking to skip if output-string empty?
+       (total .^= (+ total.^ buffer.last.count))
+       (when (<= width total.^)
+         (ejector .eject (output))))
+     (make bounded-sink
+       (to (_ .display a)   (ss .display a)   (cut-off))
+       (to (_ .write-u8 u8) (ss .write-u8 u8) (cut-off))
+       (to (_ .write a)     (a .selfie bounded-sink))
+       (to _.close          ss.close))
+     (bounded-sink .write thing)
+     (output))))
+       
+(export write-to-bounded-string)
--- library/traceback.scm
+++ library/traceback.scm
@@ -2,6 +2,7 @@
 
 ;; We try to call on less of the library than usual, to work better
 ;; when the system is borked.
+(import (use 'bounded-write) write-to-bounded-string)
 
 (to (on-error-traceback k @evil)
   (display "Error! Traceback:\n")
@@ -27,31 +28,4 @@
       (write evil)))
   (newline))
 
-;; Write `thing` into a string, but give up and truncate if it
-;; overflows `width`.
-(to (write-to-bounded-string thing width)
-  (let buffer (flexarray<-))
-  (let total (box<- 0)) ;; Kept equal to (sum _.count buffer)
-  (to (output)
-    (let s ("" .join buffer.values))
-    ;; TODO make it unambiguous whether it's truncated, somehow
-    (if (< total.^ width)
-        s
-        (chain (s .slice 0 (- width 2)) "..")))
-  (with-ejector
-   (on (ejector)
-     (let ss (string-sink<-))
-     (to (cut-off)
-       (buffer .push! ss.output-string) ;TODO worth checking to skip if output-string empty?
-       (total .^= (+ total.^ buffer.last.count))
-       (when (<= width total.^)
-         (ejector .eject (output))))
-     (make bounded-sink
-       (to (_ .display a)   (ss .display a)   (cut-off))
-       (to (_ .write-u8 u8) (ss .write-u8 u8) (cut-off))
-       (to (_ .write a)     (a .selfie bounded-sink))
-       (to _.close          ss.close))
-     (bounded-sink .write thing)
-     (output))))
-       
 (export on-error-traceback on-error-complain complain print-traceback)
