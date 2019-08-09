a76cb27c3865b306c434e5cfc56a3388402d4e2e
darius <withal@gmail.com>
2019-08-09 14:04:41 -0700

    tweak

--- library/parson.scm
+++ library/parson.scm
@@ -183,8 +183,7 @@
     (error "Ungrammatical grammar"))
 
   (let lhses (bag<- (each _.first skeletons)))
-  (let duplicates (for where ((n lhses))
-                    (< 1 n)))
+  (let duplicates (for where ((n lhses)) (< 1 n)))
   (unless duplicates.empty?
     (error "Multiply-defined rules" (sort duplicates)))
 
