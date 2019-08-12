35fd5e61b07cbf20e5fabab5e866c1a6329ec708
darius <withal@gmail.com>
2019-08-11 17:53:35 -0700

    tweak

--- library/ssets.scm
+++ library/ssets.scm
@@ -4,20 +4,16 @@
   (sset<-list xs))
 
 (to (sset<-list xs)
-  (for foldr ((x xs) (ss '()))
+  ;; TODO don't need the full sset-insert here
+  ;;  -- maybe worth adding a "sort -u" equivalent to sort.scm
+  (for foldr ((x (sort xs)) (ss '()))
     (sset-insert ss x)))
 
 (to (sset-elements xs)
   xs)
 
-(to (sset-union xs ys)
-  (merge xs ys))
-
 (to (sset-insert xs x)
-  (merge `(,x) xs))
-
-(to (sset-difference xs ys)
-  (diff xs ys))
+  (sset-union `(,x) xs))                ;TODO specialize
 
 (to (sset-remove xs unwanted)
   (may xs
@@ -28,20 +24,20 @@
           (link x1 (sset-remove xs1 unwanted))))))
 
 ;; Not quite the same as sort.scm's merge, since we dedupe here.
-(to (merge xs ys)
+(to (sset-union xs ys)
   (hm (if xs.none? ys)
       (if ys.none? xs)
       (else (may (xs.first .compare ys.first)
-              (be -1 (link xs.first (merge xs.rest ys)))
-              (be  0 (link xs.first (merge xs.rest ys.rest)))
-              (be  1 (link ys.first (merge xs ys.rest)))))))
+              (be -1 (link xs.first (sset-union xs.rest ys)))
+              (be  0 (link xs.first (sset-union xs.rest ys.rest)))
+              (be  1 (link ys.first (sset-union xs ys.rest)))))))
 
-(to (diff xs ys)
+(to (sset-difference xs ys)
   (hm (if xs.none? '())
       (if ys.none? xs)
       (else (may (xs.first .compare ys.first)
-              (be -1 (link xs.first (diff xs.rest ys)))
-              (be  0 (diff xs.rest ys.rest))
-              (be  1 (diff xs ys.rest))))))
+              (be -1 (link xs.first (sset-difference xs.rest ys)))
+              (be  0 (sset-difference xs.rest ys.rest))
+              (be  1 (sset-difference xs ys.rest))))))
 
 (export sset<- sset<-list sset-elements sset-insert sset-remove sset-union sset-difference)
