284c1e61f5752d918f42dde233973768af2b8cbf
darius <withal@gmail.com>
2019-09-06 10:30:15 -0700

    extract abcs.scm

--- cant.scm
+++ cant.scm
@@ -1,29 +1,10 @@
 #! /usr/bin/scheme --program
 #!chezscheme
 (import (chezscheme)
+  (player abcs)
   (player player))
 
-;; The Cant interpreter and global environment.
+;; The Cant interpreter
 
-(define (start-playing args)
-  (cant-interpret `(start-playing ',args)))
-
-(run-load "abcs/20-cant.cant")
-(run-load "abcs/21-sequels.cant")
-(run-load "abcs/30-functions.cant")
-(run-load "abcs/40-library.cant")
-(run-load "abcs/50-top.cant")
-(cant-interpret
- '(do
-;    (use "test/smoke-test")
-    ;; Let's default to traceback-on-error:
-    ;; TODO: also stash the error in the-last-error for below
-;    (push-signal-handler ((use 'traceback) 'on-error-traceback))
-    (import (use 'flexarray)  flexarray<- flexarray<-list)
-    (import (use 'sort)       sort sort-by)
-    (import (use 'bag)        bag<-)))
-
-;(unless (cant-interpret '(the-last-error .^))
-(start-playing (cdr (command-line)))
-
-;)
+(unless (load-abcs)
+  (cant-interpret `(start-playing ',(cdr (command-line)))))
