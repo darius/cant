380ae4054223926cc2a4a5c2b4aceef1d3738211
darius <withal@gmail.com>
2020-12-03 23:39:30 -0600

    optional cmdline parameter for the year

--- examples/advent-of-code/adventget.py
+++ examples/advent-of-code/adventget.py
@@ -3,7 +3,6 @@
 Adapted from https://github.com/ambertests/advent_of_code_2018/blob/master/aoc_day01.py
 For other ways see also https://old.reddit.com/r/adventofcode/comments/a2lesz/2018_day_3_solutions/eb1hd1z/
 To be run from ../.. (the main Cant dir).
-TODO: command-line arg to choose the year
 """
 
 import os, requests
@@ -11,14 +10,13 @@ import os, requests
 # Grab the session cookie from the https://adventofcode.com/ site:
 # right-click, Inspect, Application tab, Cookies, session.
 session = os.environ['ADVENT_SESSION']
-default_year = 2020
 
-def download(day, year=default_year):
-    text = fetch(day)
+def download(day, year):
+    text = fetch(day, year)
     with open('examples/advent-of-code/%d/data/%02d.in' % (year, day), 'w') as f:
          f.write(text)
 
-def fetch(day, year=default_year):
+def fetch(day, year):
     url = 'https://adventofcode.com/%d/day/%d/input' % (year, day)
     headers = {
 	'cookie': "session=" + session
@@ -28,6 +26,11 @@ def fetch(day, year=default_year):
 
 if __name__ == '__main__':
     import sys
+    year = 2020                 # default
+    if sys.argv[1:2] == ['-y'] and 3 <= len(sys.argv):
+        year = int(sys.argv[2])
+        del sys.argv[1:3]
     if len(sys.argv) != 2:
-        raise Exception("usage: %s day" % sys.argv[0])
-    download(int(sys.argv[1]))
+        raise Exception("usage: %s [-y year] day" % sys.argv[0])
+    day = int(sys.argv[1])
+    download(day, year)
